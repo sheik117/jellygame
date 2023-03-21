098464e71782004a1e9ce2ea6edc49536a80f9de
Fillip Novak Grunnsund <fillnova@hotmail.com>
2023-03-21 12:38:15 +0100

    add jelly

---
+++ engine/jelly.py
@@ -0,0 +1,20 @@
+class Tile:
+    def __init__(self) -> None:
+        pass
+    
+    def __init__(self, jelly):
+        pass
+
+    def get_cord():
+        pass
+
+    def get_jelly():
+        pass
+
+class Jelly(Tile):
+    def movement(x, y):
+        pass
+
+class Player(Jelly):
+    def input():
+        pass
