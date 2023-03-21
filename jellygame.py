e10d5cc9fa583daf5fe8725e7f8c2902259085f2
Ole Marcus Hansen <omhanse@hiof.no>
2023-03-21 11:33:45 +0100

    make Window class

---
+++ jellygame.py
@@ -0,0 +1,16 @@
+
+class Window:
+    def __init__(self):
+        pass  # create window
+
+    def grid(self, size_x, size_y):
+        pass  # render a grid of Tiles in the window
+
+    def loop(self, function):
+        pass  # make function run as long as the window exists
+
+    def stop_loop(self, function):
+        pass  # make function stop running
+
+    def exit(self):
+        pass  # close window and stop all loops
