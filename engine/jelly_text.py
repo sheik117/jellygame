c6d3fa4dec0cf611532e27fe61510e871eba4bbc
Radin Morik <radin.morik@gmail.com>
2023-03-21 12:04:42 +0100

    adjust comments, jell_text.py

--- engine/jelly_text.py
+++ engine/jelly_text.py
@@ -1,34 +1,26 @@
 class Jelly_text:
 
-# Creates textbox
     def textbox_create(self):
-        pass
+        pass  # Creates textbox
 
-# Sets color for textbox on front layer
     def set_front_color(self, color):
-        pass
+        pass  # Sets color for textbox on front layer
 
-# Sets text in textbox
     def set_text(self, text):
-        pass
+        pass  # Sets text in textbox
 
-# Sets the backgrund layer color in textbox
     def set_background_color(self, color):
-        pass
+        pass  # Sets the backgrund layer color in textbox
 
-# Sets the font size
     def set_font_size(self, font_size):
-        pass
+        pass  # Sets the font size
 
-# Sets the window the textbox is rendered in
     def set_window(self, window):
-        pass
+        pass  # Sets the window the textbox is rendered in
 
-# Sets how long the textbox is
     def set_span(self, span1, span2):
-        pass
+        pass  # Sets how long the textbox is
 
-# sets the position of the textbox in the grid with x,y coordinates
     def set_position(self, x, y):
-        pass
+        pass  # sets the position of the textbox in the grid with x,y coordinates
 
