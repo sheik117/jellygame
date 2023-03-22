f3e771d725855ec09fc61c6cf15ef0afa7b48d25
Fillip Novak Grunnsund <fillnova@hotmail.com>
2023-03-22 12:09:30 +0100

    add terrain

--- engine/jelly.py
+++ engine/jelly.py
@@ -13,8 +13,21 @@ class Tile:
     def get_jelly(self):
         pass
 
+    def set_jelly(self, jelly):
+        self.jelly = jelly
+    
+    def get_terrain(self):
+        pass
 
-class Jelly(Tile):
+    def set_terrain(self, terrain):
+        self.terrain = terrain
+
+class Terrain:
+    def __init__(self) -> None:
+        pass
+
+
+class Jelly():
     def __init__(self, color=c.WHITE, sprite=None, owner=0, jelly=None):
         super().__init__(self, color, sprite, jelly)
         self.owner = owner
