8989694b36cd648853aa6093d564d151b47006fc
Radin Morik <radin.morik@gmail.com>
2023-03-22 13:58:02 +0100

    add Item class

---
+++ engine/item.py
@@ -0,0 +1,13 @@
+class Item:
+    def __init__(self, name, sprite, stats):
+        self.name = name
+        self.sprite = sprite
+        self.stats = stats
+
+    def delete_item(self, item):
+        pass
+
+    def edit_item(self, item, name, sprite, stats):
+        pass
+
+    
